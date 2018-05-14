f0e9b4c3b6cadcde8182563b27e4a9f028e86ba0
Alexander Geist <age@adorsys.de>
2018-05-14 15:29:34 +0200

    new enum similarityMatchType

--- src/main/java/de/adorsys/smartanalytics/api/Matcher.java
+++ src/main/java/de/adorsys/smartanalytics/api/Matcher.java
@@ -17,11 +17,6 @@
 package de.adorsys.smartanalytics.api;
 
 public interface Matcher {
-    /**
-     * Assert the object matches this matcher.
-     *
-     * @param obj the object to match
-     * @return true if the matcher matches otherwise false.
-     */
+
     boolean match(WrappedBooking obj);
 }
