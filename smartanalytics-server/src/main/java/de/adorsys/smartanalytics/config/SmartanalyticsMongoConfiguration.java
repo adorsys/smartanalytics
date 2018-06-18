6519cdaf1a4cfbdfbdb9ef9880f01ca7861de1a7
Alexander Geist <age@adorsys.de>
2018-06-18 13:15:40 +0200

    moved smartanalytics conf files to smartanalytics-server

--- smartanalytics-server/src/main/java/de/adorsys/smartanalytics/config/SmartanalyticsMongoConfiguration.java
+++ smartanalytics-server/src/main/java/de/adorsys/smartanalytics/config/SmartanalyticsMongoConfiguration.java
@@ -1,8 +1,6 @@
 package de.adorsys.smartanalytics.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
